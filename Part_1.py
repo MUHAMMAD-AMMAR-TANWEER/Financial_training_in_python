6e865d5c9e6dc84dc127385eeb8ab6b1c978971c
Muhammad Ammar Tanweer <49334070+MUHAMMAD-AMMAR-TANWEER@users.noreply.github.com>
2021-09-13 22:33:30 +0500

    exercise 2 completed

--- Part_1.py
+++ Part_1.py
@@ -8,8 +8,17 @@ import matplotlib.pyplot as plt
 
 a = np.random.standard_normal(1000)
 b = np.array([i for i in range(1000)])
-print(a)
-plt.hist(a, bins = 10, range = (0,1), color="red", histtype="bar")
-plt.show()
-plt.plot(b,a)
+# print(a)
+# plt.hist(a, bins = 10, range = (0,1), color="red", histtype="bar")
+# plt.show()
+# plt.plot(b,a)
+# plt.show()
+
+########Exercise 2##################
+
+z = np.polyfit(b,a,1)
+
+p = np.poly1d(z)
+
+plt.scatter(p(b),b)
 plt.show()
