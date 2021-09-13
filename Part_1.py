be7ddfe8233bdff0955b85c16d48923a5d93a54b
Muhammad Ammar Tanweer <49334070+MUHAMMAD-AMMAR-TANWEER@users.noreply.github.com>
2021-09-14 00:28:47 +0500

    exercise 4 completed

--- Part_1.py
+++ Part_1.py
@@ -16,15 +16,31 @@ b = np.array([i for i in range(1000)])
 
 ########Exercise 2##################
 
-z = np.polyfit(b,a,1)
-
-p = np.poly1d(z)
-
-plt.scatter(p(b),b+a)
-plt.show()
+# z = np.polyfit(b,a,1)
+#
+# p = np.poly1d(z)
+#
+# plt.scatter(p(b),b+a)
+# plt.show()
 
 #########Exercise 3############
-k = np.cumsum(a)
+# k = np.cumsum(a)
+#
+# plt.plot(b,k)
+# plt.show()
 
-plt.plot(b,k)
-plt.show()
+
+###########Exercise 4 ########################
+
+
+from scipy.stats import multivariate_normal as mlt
+
+mean = [0,0]
+co_var = [[1,-0.5],[-0.5,2]]
+
+
+# mult_norm = mlt.rvs(mean=mean,cov=co_var , size=1000)
+# plt.scatter(mult_norm[:,0], mult_norm[:,1])
+# plt.show()
+# print(mult_norm([2,3]))
+# print(mult_norm)
